7bda720c7d2465a31e9d70683d72e3ada5a23860
itsymnsharma <itsymnsharma@gmail.com>
2026-09-06 19:49:31 +0530

    Time: 2 ms (53.08%) | Memory: 59.2 MB (81.08%) - LeetSync

--- 283-move-zeroes/move-zeroes.js
+++ 283-move-zeroes/move-zeroes.js
@@ -5,6 +5,7 @@
 var moveZeroes = function(nums) {
     let p1=0;
 
+    //We move non zeros number to start of array and fill the remaining with zeros
     for(let i=0;i<nums.length;i++){
         if(nums[i]!==0){
             nums[p1]=nums[i];
@@ -12,6 +13,7 @@ var moveZeroes = function(nums) {
         }
     }
 
+    //use Array.fill("numberTofill", startIndex, uptoIndex)
     if(p1<nums.length){
         nums.fill(0,p1,nums.length)
     }
