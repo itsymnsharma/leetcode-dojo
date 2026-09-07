c5bf579e621257f79e8947765a7e284d7aeb0ae0
itsymnsharma <itsymnsharma@gmail.com>
2026-09-07 20:08:51 +0530

    Time: 0 ms (100.00%) | Memory: 55.3 MB (73.20%) - LeetSync

--- 136-single-number/single-number.js
+++ 136-single-number/single-number.js
@@ -3,19 +3,15 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    let hash={};
+    //a xor 0 is a and a xor a is 0;
+    //like a xor b xor a xor b xor c which will results in 0 xor c aka c , we found our single number
+    let xor=0;
 
+    //we traverse through the array and do xor operations on the values
     for(let i=0; i<nums.length; i++){
-        if(!hash[nums[i]]){
-            hash[nums[i]] = 1
-        } else {
-            hash[nums[i]]++
-        }
+        //in javascript we write xor as ^
+        xor = xor ^ nums[i]
     }
 
-    for(let i=0; i<nums.length; i++){
-        if(hash[nums[i]]===1){
-            return nums[i];
-        }
-    }
+    return xor;
 };
